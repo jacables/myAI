17cead13a1dbb3d24808a3294840c4bac885896c
JOSEPH CABLES <joseph.a.cables@gmail.com>
2025-02-28 21:58:45 -0500

    Update chat.ts

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -1,7 +1,7 @@
 import { OWNER_NAME, AI_NAME } from "./identity";
 
-export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
-export const DEFAULT_RESPONSE_MESSAGE: string = `Sorry, I'm having trouble generating a response. Please try again later.`;
-export const WORD_CUTOFF: number = 8000; // Number of words until bot says it needs a break
+export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}, ${OWNER_NAME}'s AI Brand Liason. Ask me anything about our current range of excellent vehicles to which might be best for you.`;
+export const DEFAULT_RESPONSE_MESSAGE: string = `Sorry, I'm not sure I understand. Can you try asking me in another way?.`;
+export const WORD_CUTOFF: number = 1000; // Number of words until bot says it needs a break
 export const WORD_BREAK_MESSAGE: string = `[WORD BREAK MESSAGE]`;
-export const HISTORY_CONTEXT_LENGTH: number = 7; // Number of messages to use for context when generating a response
+export const HISTORY_CONTEXT_LENGTH: number = 15; // Number of messages to use for context when generating a response
